439966352179928d4e56f810632dffd4b62d1370
vidrepar <vid.repar@gmail.com>
2016-09-28 22:41:21 +0200

    Added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var serveIndex  = require('serve-index');
+var serveStatic = require('serve-static');
+
+app.use('/', serveStatic('dist'));
+app.use('/assets', serveStatic('assets'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.listen(3010, function () {
+
+    console.log('Server is running');
+
+});
